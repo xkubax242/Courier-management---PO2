d4d687a2d56d6cde2e0d2e75fa5bf74acd8f88c4
xkubax242 <kuba242@interia.pl>
2025-06-06 07:59:08 +0200

    Dodanie plików

---
+++ Courier Management/Models/Models.cs
@@ -0,0 +1,24 @@
+﻿namespace Courier_Management.Models
+{
+    public class Package
+    {
+        public int PackageID { get; set; }
+        public string Status { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int ClientID { get; set; }
+        public int? CourierID { get; set; }
+
+    }
+
+
+
+    public class Users
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+    }
+}
